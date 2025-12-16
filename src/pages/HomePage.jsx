cf47a9535aaa9f58ac76f72f1c58a46c861ee932
damian othacehe <damianothacehe@gmail.com>
2025-12-16 12:52:58 -0300

    Hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,14 @@
-export default function HomePage(){
-    return(
-        <h1>Homepage</h1>
+import { NavLink } from 'react-router-dom';
+import './HomePage.css'
+
+export default function HomePage() {
+    return (
+        <section className='hero-section'>
+            <h1 className="hero-title title-display">SOBRIO</h1>
+            <p className="hero-subtitle">Club de Vinos</p>
+            <NavLink to="/products" className="hero-cta-button">
+                Descubrí la selección de este mes
+            </NavLink>
+        </section>
     )
 }
